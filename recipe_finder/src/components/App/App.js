35bcc5fb5fdff1bd33792cff30b5630267f3be79
CGreen789 <112316339+CGreen789@users.noreply.github.com>
2023-04-21 12:31:59 +0100

    API function delivering into console

--- recipe_finder/src/components/App/App.js
+++ recipe_finder/src/components/App/App.js
@@ -1,5 +1,6 @@
 /* PASTED FROM TEST RUN
 // fetching data from https://www.themealdb.com/api/json/v1/1/random.php
+// main ingredient https://www.themealdb.com/api/json/v1/1/filter.php?i=${search} 
 // show it in the console
 // show the data on the webpage
 
@@ -14,15 +15,26 @@ async function getRecipe() {
 getRecipe();
 */
 
-import React from "react";
+import React, { useState } from "react";
 import SearchRecipe from "../SearchRecipe/index.js";
 import "./App.css";
 import RecipeList from "../RecipeList/index.js";
 
+// API function
+async function getRecipe(search) {
+  let response = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/filter.php?i=${search}`
+  );
+  let data = response.json();
+  console.log(data);
+}
+
+getRecipe();
+
 function App() {
   return (
     <div className="App">
-      <SearchRecipe />
+      <SearchRecipe getRecipe={getRecipe} />
       <RecipeList />
     </div>
   );
--- recipe_finder/src/components/SearchRecipe/index.js
+++ recipe_finder/src/components/SearchRecipe/index.js
@@ -1,32 +1,38 @@
-//  Input element - Be able to input a search term to find a desired recipe from the API 
+//  Input element - Be able to input a search term to find a desired recipe from the API
 //  Submit button - Add a search button to send the request
 //  Import a new useState
 //  Set the new state to be input and setInput (initial state as empty string)
-//  A function that saves the value of the input box to the input state 
-//  
+//  A function that saves the value of the input box to the input state
+//
 
 import { useState } from "react";
 
+function SearchRecipe({getRecipe}) {
+  const [search, setSearch] = useState("");
 
+  console.log(search);
 
-function SearchRecipe(){
-
-const [input, setInput] = useState("");
-
-console.log(input);
-
-return (
-
+  return (
     <div>
-    <label>Search Recipes:
-    <input type="text" onChange={(e) => {setInput(e.target.value)}}></input>
-    </label>
-    <button type="submit">Search</button>
-    
+      <label>
+        Search Recipes:
+        <input
+          type="text"
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+        ></input>
+      </label>
+      <button
+        type="submit"
+        onClick={() => {
+          getRecipe(search);
+        }}
+      >
+        Search
+      </button>
     </div>
-    
-)
-
+  );
 }
 
 export default SearchRecipe;
