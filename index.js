0f04e8beb82e3ba395228f6432b3789bc0f79819
CGreen789 <112316339+CGreen789@users.noreply.github.com>
2023-04-21 10:05:29 +0100

    tested API

---
+++ index.js
@@ -0,0 +1,13 @@
+// fetching data from https://www.themealdb.com/api/json/v1/1/random.php
+// show it in the console
+// show the data on the webpage
+
+async function getRecipe() {
+  let response = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/random.php"
+  );
+  let data = response.json();
+  console.log(data);
+}
+
+getRecipe();
